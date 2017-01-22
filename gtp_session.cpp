d6f6c53b7ec734dd33b9bf758324c2e2533b58a3
sunxiao2010 <sunxiao2010n@163.com>
2017-01-22 13:04:11 +0800

    first publish

--- gtp_session.cpp
+++ gtp_session.cpp
@@ -1,8 +1,8 @@
 /*
  *   BSD LICENSE
  *
- *   Copyright(c) sunxiao. All rights reserved.
- *   sunx17@chinaunicom.cn
+ *   Author: sunxiao2010n@163.com.
+ *   Copyright(c) Chinaunicom. All rights reserved.
  *
  *   Redistribution and use in source and binary forms, with or without
  *   modification, are permitted provided that the following conditions
--- gtp_session.h
+++ gtp_session.h
@@ -1,8 +1,8 @@
 /*
  *   BSD LICENSE
  *
- *   Copyright(c) sunxiao. All rights reserved.
- *   sunx17@chinaunicom.cn
+ *   Copyright(c) Unisk. All rights reserved.
+ *   Copyright(c) Chinaunicom. All rights reserved.
  *
  *   Redistribution and use in source and binary forms, with or without
  *   modification, are permitted provided that the following conditions
